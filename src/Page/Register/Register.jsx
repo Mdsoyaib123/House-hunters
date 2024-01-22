7ec7c85b9ff8d3d66f6e701b754ee17d2ffe673c
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2024-01-23 01:13:30 +0600

    implement register form

--- src/Page/Register/Register.jsx
+++ src/Page/Register/Register.jsx
@@ -1,9 +1,31 @@
+import axios from "axios";
+
 const Register = () => {
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        const form = e.target
+        const name = form.name.value ;
+        const number =form.number.value ;
+        const role = form.role.value ;
+        const email =form.email.value ;
+        const password = form.password.value ;
+        const userData = {
+            name,
+            number,
+            role,
+            email,
+            password
+        }
+        axios.post('http://localhost:5000/users',userData)
+        .then(res=>{
+            console.log(res.data);
+        })
+    }
   return (
     <div className="bg-base-300 h-screen flex justify-center items-center">
       <div className="w-1/4 text-gray-600">
         <h1 className="text-center pb-4 text-4xl">Dashboard</h1>
-        <form className=" bg-white shadow-xl w-full px-6 space-y-4 py-5 ">
+        <form onSubmit={handleSubmit} className=" bg-white shadow-xl w-full px-6 space-y-4 py-5 ">
         <img
             className="w-20 mx-auto"
             src="https://i.postimg.cc/W1kk8NZg/logo.png"
@@ -13,18 +35,21 @@ const Register = () => {
           <input
             className="w-full border px-2 py-1 "
             type="text"
-            name=""
+            required
+            name="name"
             placeholder="Full name "
           />
 
           <input
             className="w-full border px-2 py-1 "
-            type="text"
-            name=""
+            type="tel"
+            pattern="\+8801[3-9]\d{8}"
+            required
+            name="number"
             placeholder="Phone number "
           />
-          <select name="" className="w-full border px-2 py-1">
-            <option value="select" disabled>
+          <select name="role" required className="w-full border px-2 py-1">
+            <option value="Select Role" >
               Select Role{" "}
             </option>
             <option value="House Owner">House Owner</option>
@@ -33,13 +58,15 @@ const Register = () => {
           <input
             className="w-full border px-2 py-1 "
             type="email"
-            name=""
+            name="email"
+            required
             placeholder="Email "
           />
           <input
             className="w-full border px-2 py-1 "
             type="password"
-            name=""
+            required
+            name="password"
             placeholder="Password "
           />
           <input className="w-full py-1  cursor-pointer bg-blue-600 text-white " type="submit" value="Register" />
